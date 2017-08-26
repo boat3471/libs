ad557330ad54fd2279ea35bed330c094df7cb762
xinwenchao <boat3471@qq.com>
2017-08-26 09:16:15 +0800

    Update calc .10086.js

--- calc .10086.js
+++ calc .10086.js
@@ -1,20 +1,18 @@
+// x: 最大MB，y：总MB，z：总KB
 var x=0,y=0,z=0;
-var cc = $('#Searchresult_onemonthdetail001 tr td').filter(function(a, b){ 
+$('#Searchresult_onemonthdetail001 tr td').filter(function(a, b){ 
     var $b = $(b);
     if($b.text().indexOf('任我享流量半年包') >= 0){
-			var $c = $b.siblings(':eq(4)');
-			var dd = $c.text();
-			if(dd.indexOf('KB') > 0){
-				z += parseInt(dd.replace('KB', ''));
-			}
-			if(dd.indexOf('MB') > 0){
-				var yy = parseInt(dd.replace('MB', ''));
-				y += yy;
-				if(yy > x)
-					x = yy;
-			}
-      return true;
+	var $c = $b.siblings(':eq(4)');
+	var dd = $c.text();
+	if(dd.indexOf('KB') > 0){
+	    z += parseInt(dd.replace('KB', ''));
+	}
+	if(dd.indexOf('MB') > 0){
+	    var yy = parseInt(dd.replace('MB', ''));
+	    y += yy;
+	    if(yy > x) x = yy;
+	}
     }
-    return false;
 })
 console.info(x, y + z/1024)
